788b2f35562a29dfd0f993a4e2a7449426f6d3ab
prasannap <prasanna.patankar@synerzip.com>
2017-09-08 17:07:55 +0530

    primary secondary report changes

---
+++ ServoIO/ServoIO/View/InvertBoolenConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ServoIO.View
+{
+    public class InvertBoolenConverter : IValueConverter
+    {
+
+        #region IValueConverter implementation
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    if (value.ToString() == "Primary")
+                    {
+
+                        return "#6cb3fe";
+                    }
+                    return "#b0e89e";
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
